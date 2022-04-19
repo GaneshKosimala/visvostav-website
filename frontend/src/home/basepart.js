511af50f5f787a575a4eebf66308ddc5101781dd
GaneshKosimala <ganeshkosimala@gmail.com>
2022-04-19 17:47:46 +0530

    changed styling

--- frontend/src/home/basepart.js
+++ frontend/src/home/basepart.js
@@ -14,10 +14,10 @@ class Basepart extends Component {
         <div className='base'>
        <div> <h1 className='card-text text-danger display-6'>Follow Us :</h1></div>
        <div>
-           <a href='https://www.facebook.com/pbrvitsofficial/' className='socialmedia' target="_blank" rel='noreferrer noopener'>Facebook</a>
+           <a href='https://www.facebook.com/pbrvitsofficial/' className='socialmedia' target="_blank" rel='noreferrer noopener' style={{ textDecoration: 'none'}}>Facebook</a>
            </div>
            <div>
-           <a href='https://www.instagram.com/p/CcW4QjbJPZC/?igshid=MDJmNzVkMjY=' className='socialmedia' target="_blank" rel='noreferrer noopener'>Instagram</a>
+           <a href='https://www.instagram.com/p/CcW4QjbJPZC/?igshid=MDJmNzVkMjY=' className='socialmedia' target="_blank" rel='noreferrer noopener' style={{ textDecoration: 'none'}}>Instagram</a>
            </div>
         </div>
        
